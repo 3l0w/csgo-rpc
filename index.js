98e2edb46bf31fb2b9bc28ce5cf74908ef002e0b
Gwilherm Folliot <gwilherm55fo@gmail.com>
2019-02-19 13:58:55 +0100

    fixed crash in JSON.parse(data) + fixed time disappear in main menu + fixed crash in warmup + and fixed img disappear when map is unknown

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 3000;
 const host = '127.0.0.1';
 const clientId = '547104228276174869';
 const DiscordRPC = require("discord-rpc")
+const maplist = require("./maplist.json")
 DiscordRPC.register(clientId);
 var rpc = new DiscordRPC.Client({ transport: 'ipc' });
 var time
@@ -14,19 +15,21 @@ rpc.on('ready', () => {         //When rp is connected, send the username in the
 });
 rpc.connect(clientId)           //Connect the rp with the application
 
-
-
 server = http.createServer(function (req, res) {        //Create the server who listen to csgo's stats
     if (req.method == 'POST') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         var body = '';
         req.on('data', function (data) {
-            body = JSON.parse(data);
+            if (JSON.parse(data)) {
+                body = JSON.parse(data)
+            }
+
         });
         req.on('end', function () {
             console.log(body);
-            //  fs.writeFileSync("data.json", JSON.stringify(body))
-            setRpcActivity(body)
+            if (body) {
+                setRpcActivity(body)
+            }
             res.end('');
         });
     }
@@ -41,53 +44,51 @@ server = http.createServer(function (req, res) {        //Create the server who 
 var menu = false;
 server.listen(port, host);
 console.log('Listening at http://' + host + ':' + port);
-
-
+var menutime
 function setRpcActivity(data) {         //Set Rp activity
-    var activity = {                    //This var is sent to discord, it will display you stats
-        //  details: "",
-        //state: "Live", "Freezetime" or "End round"
-        //team: "ct" or "t"
-        largeImageKey: "icon",
-        largeImageText: "csgo",
-        instance: false
+    var activity = {} //This var is sent to discord, it will display you stats
 
-    }
-
-
-    if (data.player.state === undefined) {      //If the player isnt in a game, set the stats as below
-        time = undefined
-        activity.details = "In menu"
+    if (data.player.state === undefined) {    //If the player isnt in a game, set the stats as below
         if (menu === false) {
             menu = true
-            var activity = {
-                details: "In menu",
-                largeImageKey: "icon",
-                largeImageText: "csgo",
-                instance: false,
-                startTimestamp: new Date()
-
-            }
+            menutime = new Date()
         }
+        activity = {
+            details: "In menu",
+            largeImageKey: "icon",
+            largeImageText: "csgo",
+            instance: false,
+            startTimestamp: menutime
+        }
+        time = undefined
+
     }
 
 
     if (data.player.state) {        //If data.player.state isnt null, set activity with the stats below
-        
+
         if (time === undefined) {   //If the match doesnt have time code, it create one
             time = new Date()
             menu = false
         }
-        if(data.map.mode == "gungameprogressive"){
+        if (data.map.mode == "gungameprogressive") {
             var details = "Gungame" + ", " + data.player.match_stats.kills + "/" + data.player.match_stats.assists + "/" + data.player.match_stats.deaths
-        }else{
+        } else if (data.map.mode == "scrimcomp2v2") {
+            var details = "Wingman" + ", " + data.player.match_stats.kills + "/" + data.player.match_stats.assists + "/" + data.player.match_stats.deaths
+        } else {
             var details = data.map.mode[0].toUpperCase() + data.map.mode.slice(1) + ", " + data.player.match_stats.kills + "/" + data.player.match_stats.assists + "/" + data.player.match_stats.deaths
         }
-        if (data.player.team == "CT") { var team = "ct" } else { var team = "t" }
-        if (data.round.phase === "live") {
-            var phase = "Live"
+        if (data.player.team == "CT") {
+            var team = "ct"
+        } else {
+            var team = "t"
+        }
+        if (data.round === undefined) {
+            var phase = "Warmup"
         } else if (data.round.phase == "freezetime") {      //Set the match state as freezetime
             var phase = "Freeze Time"
+        } else if (data.round.phase === "live") {
+            var phase = "Live"
         } else {                                            //Set the match state as round over
             var phase = "Round Over"
         }
@@ -98,15 +99,24 @@ function setRpcActivity(data) {         //Set Rp activity
         } else {
             var state = data.map.team_ct.score + " - " + data.map.team_t.score + ", " + phase
         }
-
-
-
+        if (data.map.name === "de_shortnuke") {
+            map = "de_nuke"
+        } else {
+            map = data.map.name
+        }
+        
+        if(maplist.indexOf(map) === -1){
+            map = "icon"
+        }else{
+            maptext = map
+        }
+        
         if (data)  //If data isnt null, set the missing fields for every gamemode (such as the map, the team, etc...)
             var activity = {
                 details: details,
                 state: state,
-                largeImageKey: data.map.name,
-                largeImageText: data.map.name,
+                largeImageKey: map,
+                largeImageText: maptext,
                 smallImageKey: team,
                 instance: false,
                 startTimestamp: time
