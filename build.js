8ae051bd2f13b70ba4408e7d9bc2b07eca8475d3
Gwilherm Folliot <gwilherm55fo@gmail.com>
2019-03-05 10:23:51 +0100

    exe has now icon with resourcehacker

--- build.js
+++ build.js
@@ -1,29 +1,35 @@
 const compile = require("nexe").compile
 const fs = require("fs")
+const exec = require("child_process").execSync
 try {
     fs.mkdirSync("build")
 } catch (error) { }
 compile({
-    input: './index.js',
+    input: "./index.js",
     targets: "win",
-    cwd: "build/",
-    output: "csgorpc.exe"
+    output: "./build/csbuild.exe",
+    debugBundle: true
+}).then(() => {
+    exec("buildtools\\ResourceHacker.exe -open build\\csbuild.exe -save build\\csgorpc_win.exe -action addoverwrite -res img\\csgo_icon.ico -mask ICONGROUP,IDR_MAINFRAME", { windowsHide: true })
+    fs.unlinkSync("./build/csbuild.exe")
+
 })
 compile({
-    input: './index.js',
+    input: "./index.js",
     targets: "linux",
-    cwd: "build/",
-    output: "csgorpc"
+    output: "./build/csgorpc_linux"
 })
 compile({
-    input: './install_linux.js',
+    input: "./install_linux.js",
     targets: "linux",
-    cwd: "build/",
-    output: "install_linux"
+    output: "./build/install_linux"
 })
 compile({
-    input: './install_window.js',
+    input: "./install_windows.js",
     targets: "win",
-    cwd: "build/",
-    output: "install_window.exe"
+    output: "./build/installbuild.exe",
+    verbose: true,
+}).then(() => {
+    exec("buildtools\\ResourceHacker.exe -open build\\installbuild.exe -save build\\install_win.exe -action addoverwrite -res img\\install.ico -mask ICONGROUP,IDR_MAINFRAME", { windowsHide: true })
+    fs.unlinkSync("./build/installbuild.exe")
 })
