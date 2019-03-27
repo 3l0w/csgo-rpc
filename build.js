aaf2e160a18019264c53be95d6a703d122f8ba1c
Gwilherm Folliot <gwilherm55fo@gmail.com>
2019-03-27 15:19:35 +0100

    update to work with the tools folder

--- build.js
+++ build.js
@@ -11,11 +11,11 @@ compile({ //build csgorpc_win.exe
     debugBundle: true
 }).then(() => {
     if (process.platform === "win32") {
-        exec("buildtools\\ResourceHacker.exe -open build\\csbuild.exe -save build\\csgorpc_win.exe -action addoverwrite -res img\\csgo_icon.ico -mask ICONGROUP,IDR_MAINFRAME", { windowsHide: true })
+        exec("tools\\ResourceHacker.exe -open build\\csbuild.exe -save build\\csgorpc_win.exe -action addoverwrite -res img\\csgo_icon.ico -mask ICONGROUP,IDR_MAINFRAME", { windowsHide: true })
         //set an icon for the exe
         fs.unlinkSync("./build/csbuild.exe")
     } else if (process.platform === "linux") {
-        exec("wine buildtools\\ResourceHacker.exe -open build\\csbuild.exe -save build\\csgorpc_win.exe -action addoverwrite -res img\\csgo_icon.ico -mask ICONGROUP,IDR_MAINFRAME", { windowsHide: true })
+        exec("wine tools\\ResourceHacker.exe -open build\\csbuild.exe -save build\\csgorpc_win.exe -action addoverwrite -res img\\csgo_icon.ico -mask ICONGROUP,IDR_MAINFRAME", { windowsHide: true })
         //set an icon for the exe
         fs.unlinkSync("./build/csbuild.exe")
     }
@@ -37,11 +37,11 @@ compile({ //build install_windows.js
     verbose: true,
 }).then(() => {
     if (process.platform === "win32") {
-        exec("buildtools\\ResourceHacker.exe -open build\\installbuild.exe -save build\\install_win.exe -action addoverwrite -res img\\install.ico -mask ICONGROUP,IDR_MAINFRAME", { windowsHide: true })
+        exec("tools\\ResourceHacker.exe -open build\\installbuild.exe -save build\\install_win.exe -action addoverwrite -res img\\install.ico -mask ICONGROUP,IDR_MAINFRAME", { windowsHide: true })
         //set an icon for the exe
         fs.unlinkSync("./build/installbuild.exe")
     } else if (process.platform === "linux") {
-        exec("wine buildtools\\ResourceHacker.exe -open build\\installbuild.exe -save build\\install_win.exe -action addoverwrite -res img\\install.ico -mask ICONGROUP,IDR_MAINFRAME", { windowsHide: true })
+        exec("wine tools\\ResourceHacker.exe -open build\\installbuild.exe -save build\\install_win.exe -action addoverwrite -res img\\install.ico -mask ICONGROUP,IDR_MAINFRAME", { windowsHide: true })
         //set an icon for the exe
         fs.unlinkSync("./build/installbuild.exe")
     }
